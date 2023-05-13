701ba4a4e8b5764f5670f63e517f1eff0c1f9bf8
DelightedDuck <84408907+DelightedDuck@users.noreply.github.com>
2023-05-13 20:26:22 +0100

    fix dwl-config.h

--- suckless/dwl-config.h
+++ suckless/dwl-config.h
@@ -8,11 +8,15 @@ static const float focuscolor[]            = {1.0, 0.0, 0.0, 1.0};
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0};
 
 /* Autostart */
+
+// uncomment if using autostart patch
+/*
 static const char *const autostart[] = {
         "wbg", "/home/lain/pix/dark-street.png", NULL,
 		"yambar", NULL,
-        NULL /* terminate */
+        NULL
 };
+*/
 
 /* tagging - tagcount must be no greater than 31 */
 static const int tagcount = 9;
